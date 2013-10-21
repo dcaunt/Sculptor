49801efb6f97c7f23be489fe215c06d374698ff3
Dave Caunt <dcaunt@gmail.com>
2013-10-21 22:10:36 +0100

    Set up project Specta/Expecta and added rough tests for SCLURLMatcher

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+v0.2.1
+======
+
+* Fixed Mac project setup
+
+v0.2
+====
+
+* Initial Mac support
+
+v0.1
+====
+
+* Initial release
+* CocoaPods support
---
+++ SculptorTests/SCLTestModel.m
@@ -0,0 +1,17 @@
+//
+//  SCLTestModel.m
+//  Sculptor
+//
+//  Created by David Caunt on 21/10/2013.
+//  Copyright (c) 2013 David Caunt. All rights reserved.
+//
+
+#import "SCLTestModel.h"
+
+@implementation SCLTestModel
+
+@end
+
+@implementation SCLAlternativeTestModel
+
+@end
